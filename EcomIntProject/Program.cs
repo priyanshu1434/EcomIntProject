68ac853d4d6abeed9cf872fef3dcd1fb4ec72ca9
priyanshu1434 <2387745@cognizant.com>
2025-02-26 10:59:06 +0530

    Added cotroller testing

--- EcomIntProject/Program.cs
+++ EcomIntProject/Program.cs
@@ -4,6 +4,8 @@ using EcomIntProject.Controllers;
 using EcomIntProject.Models;
 
 ProductController product = new ProductController();
+OrderController order = new OrderController();
+
 
 Console.WriteLine("List of all entities: ");
 List<Product> data = product.readAllData();
@@ -23,3 +25,22 @@ else
 {
     Console.WriteLine("Entity not found.");
 }
+
+Console.WriteLine("\n\nList of all orders: ");
+List<Order> orderdata = order.readAllData();
+foreach (Order entityClass in orderdata)
+{
+    Console.WriteLine($"{entityClass.OrderId} | {entityClass.ProductId} | {entityClass.TotalPrice} | {entityClass.ShippingAddress} | {entityClass.OrderStatus} | {entityClass.PaymentStatus} | {entityClass.OrderDate}");
+}
+
+CartItemController cartItemController = new CartItemController();
+Console.WriteLine("\n\nFetching CartItems of a specific User with Id: ");
+List<CartItem> cartItems = cartItemController.getUserCartData(1001);
+double totalCartPrice = 0;
+foreach (CartItem cartItem in cartItems)
+{
+    totalCartPrice += cartItem.Product.Price * cartItem.Quantity;
+    Console.WriteLine($"Product: {cartItem.Product.Name}, Description: {cartItem.Product.Description}, Total Price: {cartItem.Product.Price * cartItem.Quantity} Rs, Quantity: {cartItem.Quantity}");
+
+}
+Console.WriteLine($"Total Cart Price:  {totalCartPrice} Rs");
